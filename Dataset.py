b288cc4433649078ff6f8f5a75a93373f9a5a07a
KreakxX <Henrik.standke@web.de>
2025-09-17 20:27:33 +0200

    Dataset fertig

--- Dataset.py
+++ Dataset.py
@@ -4,17 +4,15 @@ import requests
 prompts = set()
 codes = []
 
-
-
 for i in range(100):
+    print(i)
     while True:
-
         PromptResponse = requests.post(
         "http://localhost:11434/api/generate",
         json={
         "model": "phi3:3.8b",
         "prompt": """
-        Create ONE unique React button description.
+        Create ONE unique React button description.s
         Return ONLY valid JSON in this format, no extra text:
 
         {
@@ -37,62 +35,65 @@ for i in range(100):
         raw = raw.strip().removeprefix("```json").removeprefix("```").removesuffix("```").strip()
 
         import json
-        prompt_obj = json.loads(raw)   
         try:
+            prompt_obj = json.loads(raw)   
             prompt = f" {prompt_obj['component']},  {prompt_obj['bg']},  {prompt_obj['special']}, label: {prompt_obj['label']}"
             if prompt not in prompts:
                 prompts.add(prompt)
+                break
         except:
             print("failed")
 
-        CodeResponse = requests.post(
-        "http://localhost:11434/api/generate",
-        json={
-            "model": "deepseek-coder:6.7b",
-            "prompt": f"""
-Generate **only** the React button code based on this prompt:
+    CodeResponse = requests.post(
+    "http://localhost:11434/api/generate",
+    json={
+        "model": "deepseek-coder-v2:16b",
+        "prompt": f"""
+    Generate **only** the React button code based on this prompt:
 
-{prompt}
+    {prompt}
 
-Rules:
-- Use Tailwind CSS for styling.
-- Only output the <button> element in this exact format: <button className="">Label</button>
-- Do NOT include imports, React component wrappers, markdown, comments, or extra text like ``` or jsx or html before the code very Important .
-- Map the structured fields to Tailwind classes:
+    Rules:
+    - Use Tailwind CSS for styling.
+    - Only output the <button> element in this exact format: <button className="">Label</button> No explanation or Text before just keep it simple and output the Code directly
+    - Do NOT include imports, React component wrappers, markdown, comments, or extra text like ``` or jsx or html before the code very Important .
+    - Map the structured fields to Tailwind classes:
     - bg color → bg-color (Tailwind)
     - special → e.g. rounded, rounded-full, shadow
     - label → the inner text of the button
-""",
-"stream" : False
-        }
-        )
-
+    """,
+    "stream" : False
+    }
+    )
+    try:
         CodeData = CodeResponse.json()
         Code = CodeData["response"]
-        Code = Code.strip().removeprefix("jsx").removeprefix("html").removeprefix("```json").removeprefix("```").removeprefix("```jsx").removesuffix("```").strip()
+    except:
+        print("failed 2")
+    Code = Code.strip().removeprefix("jsx").removeprefix("html").removeprefix("```json").removeprefix("```").removeprefix("```jsx").removesuffix("```").strip()
 
-        if("jsx" in Code):
-            Code = Code[3:].strip()
-        Code.replace("`","")
+    if("jsx" in Code):
+        Code = Code[3:].strip()
+    Code.replace("`","")
 
-        if("hmtl" in Code):
-            Code = Code[4:].strip()
+    if("html" in Code):
+        Code = Code[4:].strip()
 
-        codes.append(Code)
-        print("->->->->->->")
-        print(prompt)
-        print("-----------")
-        print(Code)
+    codes.append(Code)
+    print("->->->->->->")
+    print(prompt)
+    print("-----------")
+    print(Code)
 
-
-
+PromptPath = os.path.join("Dataset", "prompts")
+CodePath = os.path.join("Dataset","Code")
 
 for i, prompt in enumerate(prompts, start=1):
-    file_path = os.path.join("prompts", f"{i}.txt")
+    file_path = os.path.join(PromptPath, f"{i}.txt")
     with open(file_path, "w") as f:
         f.write(prompt)
 
 for i, code in enumerate(codes,start=1):
-    file_path = os.path.join("Code", f"{i}.txt")
+    file_path = os.path.join(CodePath, f"{i}.txt")
     with open(file_path, "w") as f:
         f.write(code)
